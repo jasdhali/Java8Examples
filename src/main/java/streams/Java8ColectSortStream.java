ac53d186fbb7fbae32502eb42ef62a872b22ed8b
jaspal dhaliwal <jaspal dhaliwal>
2016-11-20 22:57:09 -0600

    Changes committed Nov 20

--- src/main/java/streams/Java8ColectSortStream.java
+++ src/main/java/streams/Java8ColectSortStream.java
@@ -10,7 +10,7 @@ public class Java8ColectSortStream {
 	public static void main(String[] args) {
 	List<String> lowCalDishNames = null;
 			List<Dish>	dishList = DishDataBase.getData( false );
-			lowCalDishNames = dishList.stream().filter(p->p.getCalories()<400).sorted( comparing(Dish::getName) ).map(Dish::getName).collect(Collectors.toList());
+			//lowCalDishNames = dishList.stream().filter(p->p.getCalories()<400).sorted( comparing(Dish::getName) ).map(Dish::getName).collect(Collectors.toList());
 			//sorted( comparing(Dish::getCalories))
 			System.out.println( lowCalDishNames  );
 	}	
